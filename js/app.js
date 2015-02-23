d2523f13fc049abd955f774c2bf8fea7b0a62e39
IgorPelevanyuk <gavelock@gmail.com>
2015-02-23 15:37:30 +0300

    Constructor added to Cat() function. Now it is Cat(data)

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-var Cat = function() {
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/prayingCat.jpeg');
-	this.imgAttribution = ko.observable('https://www.flickr.com/photos/bigtallguy/434164568');
+var Cat = function(data) {
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	//this.imgAttribution = ko.observable('https://www.flickr.com/photos/bigtallguy/434164568');
 
 	this.level = function() {
 		if (this.clickCount()<10)
@@ -20,26 +20,24 @@ var Cat = function() {
 	this.fullName = ko.computed(function() {
 		return this.name() + ' - ' + this.level();
 	}, this);
-	
+
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
 	};
 }
 var ViewModel = function() {
-	this.currentCat = ko.observable(new Cat());
-
-	
-
 	this.catsArray = ko.observableArray();
-	this.catsArray.push({'clickCount' : ko.observable(0),
-                    'name' : ko.observable('Tabby'),
-                    'imgSrc' : ko.observable('img/prayingCat.jpeg')});
-	this.catsArray.push({'clickCount' : ko.observable(0),
-                    'name' : ko.observable('Evrica'),
-                    'imgSrc' : ko.observable('img/wonderingCat.jpeg')});
-	this.catsArray.push({'clickCount' : ko.observable(0),
-                    'name' : ko.observable('Mousy'),
-                    'imgSrc' : ko.observable('img/scaredCat.jpeg')});
+	this.catsArray.push({'clickCount' : 0,
+                    'name' : 'Tabby',
+                    'imgSrc' : 'img/prayingCat.jpeg'});
+	this.catsArray.push({'clickCount' : 0,
+                    'name' : 'Evrica',
+                    'imgSrc' : 'img/wonderingCat.jpeg'});
+	this.catsArray.push({'clickCount' : 0,
+                    'name' : 'Mousy',
+                    'imgSrc' : 'img/scaredCat.jpeg'});
+
+	this.currentCat = ko.observable(new Cat(this.catsArray()[0]));
 
 }
 
